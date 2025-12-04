ecb8a89431fed25a4a04da4ed4851b8f21d429bd
Halász Ádám <hadam@mailbox.unideb.hu>
2025-12-04 22:41:24 +0100

    feat: implemented stockfish and added 20 level

--- Chess960.Web/tailwind.config.js
+++ Chess960.Web/tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     '../Chess960.Web.Client/**/*.{razor,html,cshtml}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'chess-purple': {
+          50: '#f5e6fa',
+          100: '#eacdf5',
+          200: '#d59bf0',
+          300: '#c068eb',
+          400: '#ab36e6',
+          500: '#9400cd', // Base color
+          600: '#7b00b6',
+          700: '#67009a',
+          800: '#5c008d',
+          900: '#4f0079',
+          950: '#240032',
+        },
+        'chess-dark': {
+          900: '#160020',
+          950: '#000000',
+        }
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
